c6a3ba8dae3b95c889089ce86b6e96d67ef81a46
Nolan Byrnes <nolanehs@gmail.com>
2018-01-30 09:52:43 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -5,5 +5,15 @@ class LandmarksController < ApplicationController
   erb :'/landmarks/new'
   end
 
+  post '/figures' do
+  @landmark = Landmark.create(params["landmark"])
+
+  if !params["landmark"]["name"].empty?
+    @figure.landmarks << Landmark.create(name: params["landmark"][:"name"])
+  end
+
+  @figure.save
+   redirect to "/landmarks/#{@landmark.id}"
+  end
 
 end
