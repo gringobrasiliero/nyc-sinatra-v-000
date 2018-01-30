ecf767f2ae5ec625e9b5c2c710f065f9506bd017
Nolan Byrnes <nolanehs@gmail.com>
2018-01-30 10:13:43 +0000

    automatically backed up by learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -2,6 +2,6 @@ class CreateLandmarks < ActiveRecord::Migration[5.1]
   create_table :landmarks do |t|
        t.string :name
        t.integer :figure_id
-       t.integer :year_completed
+       t.string :year_completed
 end
 end
